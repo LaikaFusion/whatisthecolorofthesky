816797e6d9209b749c0f2005a9f4b68fce458432
LaikaFusion <newrad4@gmail.com>
2018-09-28 16:05:53 -0400

    added first end point ready for db

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+const getColors = require("get-image-colors");
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.json());
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.post("/color", (req, res) => {
+  let holdingArr = req.body.images.split(",").map(async (e, i) => {
+    try {
+     return await getColors(e).then(colors=> colors.map(color => color.hex()));
+    } catch (err) {
+      console.log(err);
+    }
+  });
+  Promise.all(holdingArr).then((completed) =>  res.status(200).json({ message: completed }));
+});
+
+app.get("/", (req, res) => {
+  res.send("Wrong endpoint");
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
