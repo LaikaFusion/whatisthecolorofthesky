dd0d475358ceb159744b85b55203c66946f85d34
LaikaFusion <newrad4@gmail.com>
2018-09-28 18:06:37 -0400

    merge bugs removed

--- backend/index.js
+++ backend/index.js
@@ -1,39 +1,49 @@
 const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
 
 const app = express();
-
 
 const port = 3000;
 const getColors = require("get-image-colors");
 
-const imgur = require('./imgurPuller/imgurpuller')
+const imgur = require("./imgurPuller/imgurpuller");
 const sky = require("./routers/skyRouter");
 
 app.use(cors());
 app.use(express.json());
 app.use(helmet());
 
+app.use("/sky", sky);
+
 app.post("/color", (req, res) => {
   let holdingArr = req.body.images.split(",").map(async (e, i) => {
     try {
-     return await getColors(e).then(colors=> colors.map(color => color.hex()));
+      return await getColors(e)
+        .then(colors => colors.map(color => color.hex()))
+        .catch(err => {
+          console.log(err);
+          return;
+        });
     } catch (err) {
       console.log(err);
+      return;
     }
   });
-  Promise.all(holdingArr).then((completed) =>  res.status(200).json({ message: completed }));
+  Promise.all(holdingArr)
+    .then(completed => res.status(200).json({ message: completed }))
+    .catch(function(err) {
+      console.log(err);
+    });
 });
 
 app.get("/imgur", (req, res) => {
   imgur();
-  res.send("Wrong endpoint");
-
+  res.send("Tada");
 });
-
 
 app.get("/", (req, res) => {
   res.send("Wrong endpoint");
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
-
--- backend/routers/skyRouter.js
+++ backend/routers/skyRouter.js
@@ -2,22 +2,22 @@ const express = require("express");
 const router = express.Router();
 const dbHelpers = require("../dbHelpers/helpers");
 
-// router.get("/", async (req, res) => {
-//   let results = await dbHelpers.getSkies();
-//   res.status(200).json(results);
-// });
+router.get("/", async (req, res) => {
+  let results = await dbHelpers.getSkies();
+  res.status(200).json(results);
+});
 
-// router.get("/:id", async (req, res) => {
-//   try {
-//     let results = await dbHelpers.getSky(req.params.id);
-//     if (results.length === 0) {
-//       res.status(400).json({ error: "That ID does not exist" });
-//       return;
-//     }
-//     res.status(200).json(results);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+router.get("/:id", async (req, res) => {
+  try {
+    let results = await dbHelpers.getSky(req.params.id);
+    if (results.length === 0) {
+      res.status(400).json({ error: "That ID does not exist" });
+      return;
+    }
+    res.status(200).json(results);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
