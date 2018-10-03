8b09cad7cd85d88b096d5c4d11bc553afde3527c
LaikaFusion <newrad4@gmail.com>
2018-10-03 15:48:04 -0400

    changed color state intial value to string

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,47 @@
-import React, { Component } from 'react';
-import './App.css';
-import { HuePicker } from 'react-color';
-import Result from './components/Result/Result';
+import React, { Component } from "react";
+import "./App.css";
+import { HuePicker } from "react-color";
+import Result from "./components/Result/Result";
 
 class App extends Component {
-
   state = {
-    color: []
-  }
+    color: ""
+  };
 
-  colorSelection = (color) => {
-    this.setState({color: color.hex})
+  colorSelection = color => {
+    this.setState({ color: color.hex });
     console.log(this.state.color);
-  }
-
+  };
 
   render() {
-    console.log()
+    console.log();
     return (
-      <div className="App" style={{color: this.state.color, borderColor: this.state.color}}>
-        <i class="fas fa-cloud"></i>
-          <br /><br /><br /><br />
-          <form onSubmit={this.colorSelection}>
-              <h1>Find Your Sky</h1>
-              <p>Choose a Color:</p>
-              <br /><br />
-            <HuePicker
-              className="picker" 
-              color={this.state.color}
-              onChangeComplete={this.colorSelection}/>
-            <br /><br />
-            <button type="submit">Pick</button>
-            <br /><br />
-          </form>
-          <Result />
+      <div
+        className="App"
+        style={{ color: this.state.color, borderColor: this.state.color }}
+      >
+        <i class="fas fa-cloud" />
+        <br />
+        <br />
+        <br />
+        <br />
+        <form onSubmit={this.colorSelection}>
+          <h1>Find Your Sky</h1>
+          <p>Choose a Color:</p>
+          <br />
+          <br />
+          <HuePicker
+            className="picker"
+            color={this.state.color}
+            onChangeComplete={this.colorSelection}
+          />
+          <br />
+          <br />
+          <button type="submit">Pick</button>
+          <br />
+          <br />
+        </form>
+        <Result />
       </div>
     );
   }
